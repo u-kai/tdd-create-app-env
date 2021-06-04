eaf1e2abd18aa7b88324ecf5a195ba8d08e7b934
u-kai <76635578+u-kai@users.noreply.github.com>
2021-06-04 19:43:38 +0900

    create and clear FileMaker. clear create top package.json

---
+++ src/model/FileMaker.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs"
+
+export class FileMaker{
+    filePath:string
+    fileContents:string
+    constructor(filePath:string,fileContents:string){
+        this.filePath = filePath
+        this.fileContents = fileContents
+    }
+    writeFile = () => {
+        fs.writeFileSync(this.filePath,this.fileContents,"utf-8")
+    }
+}
--- src/tests/createEnv.test.ts
+++ src/tests/createEnv.test.ts
@@ -1,13 +1,18 @@
 import * as fs from "fs"
 import {DirectoryMaker} from "../model/DirectoryMaker"
 import * as path from "path"
+import {FileMaker} from "../model/FileMaker"
 
 
 const topDirectory = process.argv[3]
 const topMkdir = new DirectoryMaker(topDirectory)
+topMkdir.mkdir()
+
 const packageJson = "package.json"
 const topPackageJsonPath = path.resolve(topDirectory,packageJson)
-topMkdir.mkdir()
+const topPackageJsonMaker = new FileMaker(topPackageJsonPath,"")
+topPackageJsonMaker.writeFile()
+
 it("is top directory exits",()=>{
     expect(fs.existsSync(topDirectory)).toBe(true)
     expect(fs.existsSync(topPackageJsonPath)).toBe(true)
