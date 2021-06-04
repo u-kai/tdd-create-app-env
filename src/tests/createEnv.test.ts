093853900dfbd0df3c2ba53b60d002d0b61df819
u-kai <76635578+u-kai@users.noreply.github.com>
2021-06-04 19:37:38 +0900

    create new test. top package.json exist

--- src/tests/createEnv.test.ts
+++ src/tests/createEnv.test.ts
@@ -1,10 +1,14 @@
 import * as fs from "fs"
 import {DirectoryMaker} from "../model/DirectoryMaker"
+import * as path from "path"
 
 
 const topDirectory = process.argv[3]
 const topMkdir = new DirectoryMaker(topDirectory)
+const packageJson = "package.json"
+const topPackageJsonPath = path.resolve(topDirectory,packageJson)
 topMkdir.mkdir()
 it("is top directory exits",()=>{
     expect(fs.existsSync(topDirectory)).toBe(true)
+    expect(fs.existsSync(topPackageJsonPath)).toBe(true)
 })
