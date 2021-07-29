10db6050943cb934fbd9f0521f806b0f909cf325
u-kai <76635578+u-kai@users.noreply.github.com>
2021-07-30 07:37:23 +0900

    add create and install graphQL. but not test

---
+++ src/createEnvScripts/CreateTopSchema.ts
@@ -0,0 +1,15 @@
+import { ObjectType } from 'types/ObjectsType'
+import * as path from 'path'
+import { JsonFileMaker } from 'model/JsonFileMaker'
+
+export class CreateTopSchema {
+    filePath: string
+    fileData: ObjectType
+    constructor(filePath: string) {
+        this.filePath = path.resolve(filePath, 'schema.graphql')
+        this.fileData = {}
+    }
+    createFile = (): void => {
+        return new JsonFileMaker(this.filePath, this.fileData).writeFile()
+    }
+}
---
+++ src/install-packages/InstallGraphQLServers.ts
@@ -0,0 +1,10 @@
+export const InstallGraphQl = [
+    '@graphql-tools/load',
+    '@graphql-toos/schema',
+    '@graphql-tools/graphql-file-loader',
+    'graphql',
+    'apollo-server',
+    '@graphql-codegen/cli ',
+    '@graphql-codegen/typescript ',
+    '@graphql-codegen/typescript-resolvers ',
+]
